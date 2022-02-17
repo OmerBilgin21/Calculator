3da46a1cca44a7a2f7329aa5c374a22c409cce16
OmerBilgin21 <omrfrkblgn21@gmail.com>
2022-02-17 22:42:07 +0300

    necessary item creation

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const Calculator = () => {
+    return (
+        <section>
+            <div className="container">
+
+
+
+                <input type="" class="screen" />
+
+                
+                    <div className="first">
+                        <button class="span-three">c</button>
+                        <button>/</button>
+                    </div>
+
+                    <div className="second">
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                        <button className='operator'>*</button>
+                    </div>
+
+                    <div className="third">
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                        <button className='operator'>-</button>
+                    </div>
+
+
+                    <div className="fourth">
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button className='operator'>+</button>
+                    </div>
+
+                    <div className="fifth">
+                        <button class="span-three">0</button>
+                        <button>=</button>
+                    </div>
+                
+
+
+
+            </div>
+        </section>
+    );
+}
+
+export default Calculator;

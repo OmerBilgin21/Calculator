152b56c0d79a202f3a26527001285122540a7758
OmerBilgin21 <omrfrkblgn21@gmail.com>
2022-02-17 18:12:21 +0300

    Creation

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Calculator = () => {
+    return (
+        <section>
+            <div class="container">
+                
+            </div>
+        </section>
+
+    );
+}
+
+export default Calculator;

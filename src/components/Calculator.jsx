3dec1ac2c8133d01c3d4aecde90af3268acedb8f
OmerBilgin21 <omrfrkblgn21@gmail.com>
2022-02-18 13:54:15 +0300

    setting up the appearance

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,46 +5,40 @@ const Calculator = () => {
         <section>
             <div className="container">
 
-                
-
                 <input type="" class="screen" />
 
-
                     <div className="first">
-                        <button class="span-three">c</button>
-                        <button>/</button>
+                        <button className="span-three">c</button>
+                        <button className="span-one operator">/</button>
                     </div>
-
+                    
                     <div className="second">
-                        <button>7</button>
-                        <button>8</button>
-                        <button>9</button>
-                        <button className='operator'>*</button>
+                        <button className="span-one">7</button>
+                        <button className="span-one">8</button>
+                        <button className="span-one">9</button>
+                        <button className="span-one operator">*</button>
                     </div>
 
                     <div className="third">
-                        <button>4</button>
-                        <button>5</button>
-                        <button>6</button>
-                        <button className='operator'>-</button>
+                        <button className="span-one">4</button>
+                        <button className="span-one">5</button>
+                        <button className="span-one">6</button>
+                        <button className="span-one operator">-</button>
                     </div>
 
 
                     <div className="fourth">
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button>
-                        <button className='operator'>+</button>
+                        <button className="span-one">1</button>
+                        <button className="span-one">2</button>
+                        <button className="span-one">3</button>
+                        <button className="span-one operator">+</button>
                     </div>
 
                     <div className="fifth">
-                        <button class="span-three">0</button>
-                        <button>=</button>
+                        <button className="span-three">0</button>
+                        <button className="span-one operator">=</button>
                     </div>
                 
-
-
-
             </div>
         </section>
     );
